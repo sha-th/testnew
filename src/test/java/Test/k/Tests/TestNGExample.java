e13899a0a46fc682941215dcbfaa8149d284d9a2
Your Name1 <you@example1.com>
2025-05-19 14:55:32 +0530

    add mssg

---
+++ src/test/java/Test/k/Tests/TestNGExample.java
@@ -0,0 +1,11 @@
+package Test.k.Tests;
+
+import org.testng.annotations.Test;
+
+public class TestNGExample {
+    
+    @Test
+    public void testMethod() {
+        System.out.println("This is a TestNG test case.");
+    }
+}
